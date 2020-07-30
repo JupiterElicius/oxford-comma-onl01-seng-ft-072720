732d242fa9ff2ebe6eca6242f8e3961a77bbe818
Nick Bazaldua <bazalnick@gmail.com>
2020-07-30 23:06:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
+require 'pry'
 def oxford_comma(array)
   
   if array.length == 1 
     array.join(" , ")
   elsif array.length == 2 
     array.join(" and ")
-  elsif array.length == 3 
+  else
+    binding.pry
     array.insert("")
     
     
